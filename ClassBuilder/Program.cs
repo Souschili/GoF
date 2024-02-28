9f4f424078c8612e20132bad75dbbe185990cc57
Orkhan <aliyevorhan82@gmail.com>
2024-02-28 13:11:27 +0400

    Add project files.

---
+++ ClassBuilder/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Text;
+// Builder for generate class and fields
+// test passed 28.02.2024 by Orkhan 
+namespace ClassBuilder
+{
+    class ClassElement
+    {
+        public string Type { get; set; } = default!;
+        public string Name { get; set; } = default!;
+        public string Access { get; set; } = default!;
+
+         const int STEP = 2;
+
+        public List<ClassElement> ClassElements= new List<ClassElement>();
+        public ClassElement() { }
+
+        public ClassElement(string type, string name, string access)
+        {
+            Type = type;
+            Name = name;
+            Access = access;
+        }
+
+      
+        // print classelem  
+        public override string ToString()
+        {
+            var i = new string(' ', STEP);
+            var sb = new StringBuilder();
+            sb.Append($"{this.Access} {this.Type} {this.Name}\n");
+            sb.Append("{\n");
+            foreach (var  e in ClassElements)
+            {
+                sb.Append($"{i}{e.Access} {e.Type} {e.Name}");
+                sb.Append(Environment.NewLine);
+            }
+            sb.Append("}\n");
+            return sb.ToString();
+        }
+    }
+
+    class CodeBuilder
+    {
+        public readonly string rootName;
+        public readonly string rootAccess;
+        public readonly string rootType;
+
+        protected ClassElement root=new ClassElement();
+
+        public CodeBuilder(string rootName)
+        {
+            this.rootName = rootName;
+            root.Name = rootName;
+            this.rootAccess = "public";
+            root.Access = this.rootAccess;
+            this.rootType = "class";
+            root.Type = this.rootType;
+            
+        }
+       
+        //Fluent 
+        public CodeBuilder AddField(string childName,string childType,string childAccess="public")
+        {
+            var e= new ClassElement(childType,childName,childAccess);
+            // add field to list
+            root.ClassElements.Add(e);
+            return this;
+        }
+
+        public override string ToString()=> root.ToString();
+
+        public void Clean()
+        {
+            root = new ClassElement{ Name = rootName,Access=rootAccess,Type=rootType};
+        }
+
+        public ClassElement Build() => root;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new CodeBuilder("Person")
+                .AddField("Name", "string")
+                .AddField("Age","int");
+                
+            Console.WriteLine(builder);
+        }
+    }
+}
---
+++ Inherite/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Channels;
+
+namespace Inherite
+{
+
+    // super base class
+    class Person
+    {
+        private string _name;
+        public string Name { get { return _name; }set { _name = value;} }
+
+        public void Print() => Console.WriteLine(_name);
+
+
+        public override string ToString()
+        {
+            return this._name;
+        }
+    }
+    //607 Aida
+    // 799-607=192
+    class Employee:Person
+    {
+        public void Print()
+        {
+            Console.WriteLine(this.Name);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //наследование реализует отношение is-a (является),
+            //объект класса Employee также является объектом класса Person:
+            Person person = new Person { Name = "Tom" };
+            person.Print();   // Tom
+            person = new Employee { Name = "Sam" };
+            person.Print();   // Sam
+        }
+    }
+}
