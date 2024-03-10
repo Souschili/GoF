568ad3ab4b21c7175788f2e4074a0de8bd927426
Orkhan <aliyevorhan82@gmail.com>
2024-03-10 07:00:51 +0400

    remove emty app and add new console app

---
+++ MultifacetedBuilder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MultifacetedBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- UpCasting/Program.cs
+++ UpCasting/Program.cs
@@ -2,6 +2,37 @@
 {
     internal class Program
     {
+        class Person
+        {
+            public string Name { get; set; }
+            public Person(string name)
+            {
+                Name = name;
+            }
+            public void Print()
+            {
+                Console.WriteLine($"Person {Name}");
+            }
+        }
+
+        class Employee : Person
+        {
+            public string Company { get; set; }
+            public Employee(string name, string company) : base(name)
+            {
+                Company = company;
+            }
+        }
+
+        class Client : Person
+        {
+            public string Bank { get; set; }
+            public Client(string name, string bank) : base(name)
+            {
+                Bank = bank;
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
