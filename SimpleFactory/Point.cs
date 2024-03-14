b7e0710ea3803c3ac6f68fa98194c4549c3e32b3
Orkhan <aliyevorhan82@gmail.com>
2024-03-14 05:22:33 +0400

    done simple factory & included factory

---
+++ SimpleFactory/Point.cs
@@ -0,0 +1,32 @@
+﻿namespace SimpleFactory
+{
+    public class Point
+    {
+        private double x, y;
+
+        private Point(double x,double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        // add new static  instance
+        // now we not use Point.PointFactory ...
+        // we can use Point.Factory.CartesianPoint(x,y)
+        public static PointFactory Factory = new PointFactory();
+
+        // include factory into Point class and make ctor private :0)
+        public class PointFactory
+        {
+            public static Point NewCartezianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+
+            public static Point NewPolarPoint(double rho, double theta)
+            {
+                return new Point(rho * Math.Sin(rho), theta * Math.Sin(theta));
+            }
+        }
+
+    }
+}
