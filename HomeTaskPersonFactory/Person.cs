b26a8a67db4319b39e590a8ca2179f06bbda2660
Orkhan <aliyevorhan82@gmail.com>
2024-03-15 09:10:18 +0400

    finish home task

---
+++ HomeTaskPersonFactory/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace HomeTaskPersonFactory
+{
+    public class Person
+    {
+        static int _id;
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public Person()
+        {
+            this.Id = _id;
+            _id++;
+        }
+    }
+}
---
+++ HomeTaskPersonFactory/PersonFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTaskPersonFactory
+{
+    public class PersonFactory
+    { 
+        public Person CreatePerson(string name)
+        {
+            return new Person {Name=name};
+        }
+    }
+}
