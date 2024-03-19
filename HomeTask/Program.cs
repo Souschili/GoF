036d81bd448af2afef885a558f98f0138043f634
Orkhan <aliyevorhan82@gmail.com>
2024-03-19 22:20:29 +0400

    done make solution more optimal

---
+++ HomeTask/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace HomeTask
+{
+    public class Point
+    {
+        public int X, Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+
+        }
+
+        public override string ToString()
+        {
+            return $"X: {this.X}\tY: {this.Y} ";
+        }
+    }
+
+    public class Line
+    {
+        public Point Start, End;
+
+        public Line DeepCopy()
+        {
+            var over = new Line();
+            over.Start = new Point(this.Start.X,this.Start.Y);
+            over.End = new Point(this.End.X,this.End.Y);
+            return over;
+
+        }
+        public override string ToString()
+        {
+            return $"Start :{Start}\t End: {End}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Line foo1 = new Line();
+            foo1.Start = new Point(100, 25);
+            foo1.End = new Point(75, 65);
+
+            Line foo2 = foo1.DeepCopy();
+            foo2.Start.X = 120;
+            foo2.Start.Y = 45;
+            Console.WriteLine(foo1);
+            Console.WriteLine(foo2);
+
+        }
+    }
+}
