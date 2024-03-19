52522bece8ebc3a11658dd7dd0156fcff9681f78
Orkhan <aliyevorhan82@gmail.com>
2024-03-19 04:25:47 +0400

    done

--- SerializationCopy/Program.cs
+++ SerializationCopy/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace SerializationCopy
+﻿using System.Net.WebSockets;
+using System.Xml.Serialization;
+
+namespace SerializationCopy
 {
+    // good variant
+
+    static class ExtentionMethod
+    {
+        public static T DeepCopyXml<T>(this T obj)
+        {
+            
+
+            using var ms = new MemoryStream();
+            var ser = new XmlSerializer(typeof(T));
+            ser.Serialize(ms, obj);
+            ms.Position = 0;
+
+            return (T)ser.Deserialize(ms)!;
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return $"{Id} {Name}";
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var user = new User { Id = 1, Name = "Demo" };
+            var user2=user.DeepCopyXml();
+            user2.Id = 10;
+            user2.Name = "Demo2";
+            Console.WriteLine(user);
+            Console.WriteLine(user2);
         }
     }
 }
