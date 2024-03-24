78cf509d4bdc81465fa8d5f5fb2cfacf45af7fa0
Orkhan <aliyevorhan82@gmail.com>
2024-03-24 09:28:22 +0400

    add multitone

---
+++ Multitone/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Multitone
+{
+
+    public enum SubSystem
+    {
+        Main,
+        Backup
+    }
+
+    public class Printer
+    {
+        private Printer() { }
+        public static Printer Get(SubSystem ss)
+        {
+            if(_instance.ContainsKey(ss))
+                return _instance[ss];
+            _instance[ss]= new Printer();
+            return _instance[ss];
+                
+        }
+        private static readonly IDictionary<SubSystem, Printer> _instance=
+            new Dictionary<SubSystem,Printer>();
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var pr1=Printer.Get(SubSystem.Main);
+            var pr2=Printer.Get(SubSystem.Backup);
+            var pr3=Printer.Get(SubSystem.Backup);
+
+            Console.WriteLine(ReferenceEquals(pr2,pr3));
+        }
+    }
+}
